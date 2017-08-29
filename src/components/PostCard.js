51a58fb1524d065cf83438a4a3cbe3a44ea48189
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-08-30 00:13:10 +0700

    Rearrange vote score and timestamp

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,44 +1,41 @@
 import React from 'react'
 import moment from 'moment'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
-const Thumbs = styled.span`
-  cursor: pointer
-`
+const Thumbs = styled.span`cursor: pointer;`
 
 const PostCard = ({ post, votePost, index }) => {
 	return (
 		<div className="card my-2">
 			<div className="card-body">
-				<h4 className="card-title">
-					{post.title}
-				</h4>
-				<h6 className="card-subtitle mb-2 text-muted">
-					{post.author}
-				</h6>
-				<p className="card-text">
-					{post.body}
-				</p>
+				<Link to={`${post.category}/${post.id}`}>
+					<h4 className="card-title">{post.title}</h4>
+				</Link>
+
+				<h6 className="card-subtitle mb-2 text-muted">{post.author}</h6>
+				<p className="card-text">{post.body}</p>
 			</div>
 
 			<div className="card-footer">
-				<small className="text-muted float-left">
-					{moment(post.timestamp).format('L')}{' '}
-					{moment(post.timestamp).format('LT')}
-				</small>
-
-				<small className="float-right">
-					<Thumbs className="mx-3" onClick={() => votePost(post.id, 'upVote')}>
+				<small className="float-left">
+					<Thumbs className="mr-3" onClick={() => votePost(post.id, 'upVote')}>
 						<i className="fa fa-thumbs-o-up" aria-hidden="true" />
 					</Thumbs>
 					<span>
-						<b>
-							{post.voteScore}
-						</b>
+						<b>{post.voteScore}</b>
 					</span>
-					<Thumbs className="mx-3" onClick={() => votePost(post.id, 'downVote')}>
+					<Thumbs
+						className="ml-3"
+						onClick={() => votePost(post.id, 'downVote')}
+					>
 						<i className="fa fa-thumbs-o-down" aria-hidden="true" />
 					</Thumbs>
+				</small>
+
+				<small className="text-muted float-right">
+					{moment(post.timestamp).format('L')}{' '}
+					{moment(post.timestamp).format('LT')}
 				</small>
 			</div>
 		</div>
