2f38624455ae3ec6b11e44b9c5966da4e2b65157
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-09-10 23:41:05 +0700

    Refactor create post and comment reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,61 +3,8 @@ import category from './category'
 import post from './post'
 import sort from './sort'
 import comment from './comment'
-import {
-	CREATE_NEW_POST,
-	CLEAR_NEW_POST,
-	CREATE_NEW_COMMENT
-} from '../actions'
-import _ from 'lodash'
-
-const initialCreateState = {
-	id: '',
-	timestamp: '',
-	title: '',
-	body: '',
-	author: '',
-	category: 'react'
-}
-
-export const create = (state = initialCreateState, action) => {
-	switch (action.type) {
-		case CREATE_NEW_POST:
-			return {
-				...state,
-				[action.section]: action.payload
-			}
-
-		case CLEAR_NEW_POST:
-			return initialCreateState
-
-		default:
-			return state
-	}
-}
-
-const initialCreateCommentState = {
-	id: '',
-	timestamp: '',
-	body: '',
-	author: '',
-	parentId: ''
-}
-
-export const createComment = (state = initialCreateCommentState, action) => {
-	switch (action.type) {
-		case CREATE_NEW_COMMENT:
-			return {
-				...state,
-				[action.section]: action.payload
-			}
-
-		case CLEAR_NEW_POST:
-			return initialCreateCommentState
-
-		default:
-			return state
-	}
-}
+import create from './create'
+import createComment from './createComment'
 
 const reducer = combineReducers({
 	category,
