1ce45c2675627e2c410aae6f3f3ade8f86a2f5c5
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-11-23 02:07:44 -0800

    Restructuring project folder

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,12 +23,15 @@ module.exports = {
     'comma-dangle': 0,
     'max-len': 0,
     'import/extensions': 0,
+    'import/no-named-as-default': 0,
+    'import/no-named-as-default-member': 0,
     'no-underscore-dangle': 0,
     'consistent-return': 0,
     'react/display-name': 1,
     'react/react-in-jsx-scope': 0,
     'react/forbid-prop-types': 0,
     'react/no-unescaped-entities': 0,
+    'no-undef': 0,
     'react/jsx-filename-extension': [
       1,
       {
--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, compose, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import registerServiceWorker from './utils/registerServiceWorker';
-import rootReducer from './reducers';
+import store from './store/store';
 import App from './containers/App';
 import CategoryContainer from './containers/CategoryContainer';
 import PostContainer from './containers/PostContainer';
 import CreatePostContainer from './containers/CreatePostContainer';
 import EditPostContainer from './containers/EditPostContainer';
-import Navbar from './components/Navbar';
+import Navbar from './components/Header/Navbar/Navbar';
 import './index.css';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
